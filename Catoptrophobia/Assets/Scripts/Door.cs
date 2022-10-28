2d59a98539654c62fc3c708907c78041b753ecb7
yusin03 <ingmuyoug@gmail.com>
2022-10-28 23:34:51 +0900

    2022/10/28/yusin
    포탈씬 추가

---
+++ Catoptrophobia/Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool open = false;//문 열림/닫힘
+    public float doorOpenAngle = 90f;//열린 문 각도
+    public float doorCloseAngle = 0f;//닫힌 문 각도
+    public float smoot = 2f;//문 열리는 속도
+
+    public void ChangeDoorState()//문 상태 바꾸는 함수
+    {
+        open = !open;
+    }
+    void Update()
+    {
+        DoorMovement();
+    }
+    void DoorMovement()//문 여닫는 함수
+    {
+        if (open)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, doorOpenAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smoot * Time.deltaTime);
+        }
+        else
+        {
+            Quaternion targetRotation2 = Quaternion.Euler(0, doorCloseAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smoot * Time.deltaTime);
+        }
+    }
+}
